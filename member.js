6bd18a3e7facc2c61fb530adff79bed9abaddf2c
DariusDabert <115560159+DariusDabert@users.noreply.github.com>
2023-11-14 10:40:09 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var memberSkills = document.getElementById("member-skills");
+  var memberSkillsButton = document.getElementById("member-skills-button");
+
+  if (memberSkills.style.display === "none") {
+    memberSkills.style.display = "block";
+    memberSkillsButton.innerHTML = "Hide Skills";
+    member.style.height = "auto";
+    member.style.paddingBottom = "20px";
+  } else {
+    memberSkills.style.display = "none";
+    memberSkillsButton.innerHTML = "Show Skills";
+    member.style.height = "auto";
+    member.style.paddingBottom = "0px";
+  }
+}
